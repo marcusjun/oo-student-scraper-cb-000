66710bc7a6d7c07c14282d74d1612c592ecc8a91
Marcus Jun <mjun@calstate.edu>
2018-05-12 17:44:20 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,21 +9,33 @@ class Scraper
 
     index_students=Nokogiri::HTML(html)
 
-    students=[]
-
-    #index_students.css("div.roster-cards-container").each do |student|
-    index_students.css("div.roster-cards-container div.student-card").each do |student|
-      students << {
+    #Using #collect to return a new array of the student hashes
+    index_students.css("div.student-card").collect do |student|
+      {
         :name => student.css("div.card-text-container h4.student-name").text,
         :location => student.css("div.card-text-container p.student-location").text,
-        #:profile_url => student.css("div.student-card a href")#.value#.attribute("href")).value
         :profile_url => student.css("a").attribute("href").value
-
       }
     end
 
-    students
+    ###################################################################################
+    #Code below works but relies on creating and returning the local variable, students
 
+    #students=[]
+
+    #Either line of code works
+    #index_students.css("div.student-card").each do |student|
+    #index_students.css("div.roster-cards-container div.student-card").each do |student|
+      #students << {
+        #:name => student.css("div.card-text-container h4.student-name").text,
+        #:location => student.css("div.card-text-container p.student-location").text,
+        ##:profile_url => student.css("div.student-card a href")#.value#.attribute("href")).value
+        #:profile_url => student.css("a").attribute("href").value
+
+      #}
+    #end
+
+    #students
   end
 
   def self.scrape_profile_page(profile_url)
@@ -35,12 +47,12 @@ class Scraper
 
     student_profile={
       :profile_quote => student_page.css("div.profile-quote").text,
-      :bio => student_page.css("div.bio-content.content-holder div.description-holder").text.gsub("\n","").gsub("  ",""),
+      :bio => student_page.css("div.bio-content.content-holder div.description-holder p").text.gsub("\n","").gsub("  ",""),
       ##:twitter => student_page.css("div.social-icon-container a").attribute("href").value,
       ##:linkedin => student_page.css("div.social-icon-container a").attribute("href").value,
       ##:github => student_page.css("div.social-icon-container a").attribute("href").value,
       ##:blog => student_page.css("div.social-icon-container a").attribute("href").value
-    }
+      }
       ##student_page.css("div.social-icon-container").each do |social_media|
       student_page.css("div.social-icon-container a").each do |social_media|
         ##:profile_quote => student_page.css("div.profile-quote").text,
@@ -63,9 +75,10 @@ class Scraper
           ##student_profile[:github] = social_media.css("a").attribute("href").value
           student_profile[:github] = social_media.attribute("href").value
         else
+        ##elsif social_media.attribute("href").value != nil
           ##:blog => social_media.css("a").attribute("href").value
           ##student_profile[:blog] = social_media.css("a").attribute("href").value
-          student_profile[:blog] = social_media.attribute("href").value
+          student_profile[:blog] = social_media.attribute("href").value ##if social_media.attribute("href").value != nil
         end
       end
 
--- lib/student.rb
+++ lib/student.rb
@@ -5,28 +5,46 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    student_hash.each do |key,value|
-      #self.send(key,value)
-      self.send(key.to_s+"=",value)
-    end
+
+    student_hash.each {|key,value| self.send(key.to_s+"=",value)}
+
+    #Code below does the same thing
+    #student_hash.each do |key,value|
+      ##self.send(key,value)
+      #self.send(key.to_s+"=",value)
+    #end
 
     @@all << self
+
+    ##########################################################################
+    #Wish I could do this...
+    #@@all << student_hash.collect {|key,value| self.send(key.to_s+"=",value)}
 
   end
 
   def self.create_from_collection(students_array)
-    students_array.each do |student|
+    students_array.each {|student| self.new(student)}
+
+    #Code below does the same thing
+    #students_array.each do |student|
       #Either code works
       #Student.new(student)
-      self.new(student)
-    end
+      #self.new(student)
+    #end
   end
 
   def add_student_attributes(attributes_hash)
-    attributes_hash.each do |key,value|
-      self.send(key.to_s+"=",value)
-    end
-    self
+    attributes_hash.collect {|key,value| self.send(key.to_s+"=",value)}
+
+    #Code below does the same thing
+    #attributes_hash.each {|key,value| self.send(key.to_s+"=",value)}
+    #self
+
+    #Code below does the same thing
+    #attributes_hash.each do |key,value|
+      #self.send(key.to_s+"=",value)
+    #end
+    #self
   end
 
   def self.all
