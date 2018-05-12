bb6062a73006df8c7e0e009e48d7110b7351e20c
Marcus Jun <mjun@calstate.edu>
2018-05-12 16:16:10 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,6 +8,15 @@ class Scraper
     html=File.read(index_url)
 
     index_students=Nokogiri::HTML(html)
+    
+    #Using #collect to return a new array of the student hashes
+    index_students.css("div.student-card").collect do |student|
+      {
+        :name => student.css("div.card-text-container h4.student-name").text,
+        :location => student.css("div.card-text-container p.student-location").text,
+        :profile_url => student.css("a").attribute("href").value
+      }
+    end
 
     #students=[]
 
@@ -25,13 +34,7 @@ class Scraper
 
     #students
 
-    index_students.css("div.student-card").collect do |student|
-      {
-        :name => student.css("div.card-text-container h4.student-name").text,
-        :location => student.css("div.card-text-container p.student-location").text,
-        :profile_url => student.css("a").attribute("href").value
-      }
-    end
+
   end
 
   def self.scrape_profile_page(profile_url)
