0a2a515d07ccb512c2d86cba34c254f15970ffb8
Marcus Jun <mjun@calstate.edu>
2018-05-12 17:02:35 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,8 +6,8 @@ class Student
 
   def initialize(student_hash)
     student_hash.each do |key,value|
-      self.send(key,value)
-      #self.send(key.to_s+"=",value)
+      ##self.send(key,value)
+      self.send(key.to_s+"=",value)
     end
 
     @@all << self
