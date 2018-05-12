d80485328a48fc73fc55c15ec73ae48ad5f362a5
Marcus Jun <mjun@calstate.edu>
2018-05-12 17:13:51 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,11 +18,14 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    students_array.each do |student|
+    students_array.each {|student| self.new(student)}
+    
+    #Code below does the same thing
+    #students_array.each do |student|
       #Either code works
       #Student.new(student)
-      self.new(student)
-    end
+      #self.new(student)
+    #end
   end
 
   def add_student_attributes(attributes_hash)
