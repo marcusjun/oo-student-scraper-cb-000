e1c92f8fb8e85c438f03eba5f07ca27ddbc76b15
Marcus Jun <mjun@calstate.edu>
2018-05-12 17:28:26 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,6 +35,9 @@ class Student
 
   def add_student_attributes(attributes_hash)
     attributes_hash.collect {|key,value| self.send(key.to_s+"=",value)}
+
+    #Code below does the same thing
+    #attributes_hash.each {|key,value| self.send(key.to_s+"=",value)}
     #self
 
     #Code below does the same thing
